e01c7d259b85d114fd6de7e3cd194b96fe01e202
Дмитрий Коптик <43681022+DmitriyKoptik@users.noreply.github.com>
2019-12-24 04:59:47 +0300

    another change

--- framework/src/test/java/test/IncorrectSearchHotelTest.java
+++ framework/src/test/java/test/IncorrectSearchHotelTest.java
@@ -6,10 +6,114 @@ import org.testng.annotations.Test;
 import page.HotelHomePage;
 import service.HotelSearchOptionsCreator;
 
-import static util.StringUtils.CITY_NAME_ALERT_TEXT;
-import static util.StringUtils.DATA_INPUT_ALERT_TEXT;
+import static util.StringUtils.*;
+import static util.StringUtils.NEW_DESTINATION;
 
 public class IncorrectSearchHotelTest extends CommonCondition  {
+
+    @Test
+    public void destinationValidityInSearchResult(){
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        String destination = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .getDestination();
+
+        Assert.assertEquals(CORRECT_CITY_NAME, destination);
+    }
+
+    @Test
+    public void registrationFormAppearsForUnregisteredUser() {
+        boolean hasFormAppeared = new HotelHomePage(driver)
+                .openPage()
+                .checkReservations()
+                .isRegistrationFormExists();
+
+        Assert.assertFalse(hasFormAppeared);
+    }
+
+    @Test
+    public void checkSuggestionListAfterSearch(){
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        String  message = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .redirectToHotelHomePage()
+                .destinationSuggestion();
+
+        Assert.assertEquals(RECENT_SEARCH, message);
+    }
+
+    @Test
+    public void chooseMainPageLanguageStoresInCookies() {
+        String bannerContent = new HotelHomePage(driver)
+                .openPage()
+                .changeSystemLang()
+                .refreshPage()
+                .getUpdatedMainHeroBanner();
+
+        Assert.assertEquals(ITALIAN_BANNER_CONTENT, bannerContent);
+    }
+
+    @Test
+    public void changeDestinationInSearchResults() {
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        String changedDestination = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .changeDestination(hotelSearchOptions.getNewDestination())
+                .getDestination();
+
+        Assert.assertEquals(NEW_DESTINATION, changedDestination);
+    }
+
+    @Test
+    public void rightDestinationIsPointedOnAMap() {
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        boolean destinationOnMapEquals= new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .rightDestinationIsPinnedOnMap();
+
+        Assert.assertTrue(destinationOnMapEquals);
+    }
+
+    @Test
+    public void priceOfTheHotelAfterSearchingAreTheSameAsOnTheHotelPage() {
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        String priceAmountInSearchResult = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .getPriceFromSearchResult();
+
+        String pointsAmountInHotelDetail = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .redirectToHotelDetailPage()
+                .getPriceFromHotelDetail();
+
+        Assert.assertEquals(priceAmountInSearchResult, pointsAmountInHotelDetail);
+    }
+
+    @Test
+    public void availabilityOfGuestInfoInputFieldForUnregistredUsers() {
+        HotelSearchOptions hotelSearchOptions = HotelSearchOptionsCreator.withCorrectSearch();
+        boolean hasGuestInfoInputFieldAppeared = new HotelHomePage(driver)
+                .openPage()
+                .searchForHotels(hotelSearchOptions.getDestination())
+                .submitHotelSearch()
+                .redirectToHotelDetailPage()
+                .redirectToBookingHotelPage()
+                .isGuestInfoExists();
+
+        Assert.assertFalse(hasGuestInfoInputFieldAppeared);
+    }
 
     @Test
     public void IncorrectDestinationName(){
@@ -34,4 +138,5 @@ public class IncorrectSearchHotelTest extends CommonCondition  {
                 .invalidDataInput();
         Assert.assertEquals(DATA_INPUT_ALERT_TEXT, alertText);
     }
+
 }
